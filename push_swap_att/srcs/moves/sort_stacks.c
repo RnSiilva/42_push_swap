0308aa95d1166d8a04584efbde4631a2297a3abf
RnSiilva <renan.carlossilva@hotmail.com>
2023-12-12 23:13:46 +0000

    att_1.2

---
+++ push_swap_att/srcs/moves/sort_stacks.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_stacks.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: resilva <resilva@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 22:20:33 by resilva           #+#    #+#             */
+/*   Updated: 2023/12/10 02:42:16 by resilva          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+static void	min_to_top(t_stack_node **a)
+{
+	t_stack_node	*min;
+
+	min = find_min(*a);
+	while ((*a)->nbr != min->nbr)
+	{
+		if (min->above_median)
+			ra(a, false);
+		else
+			rra(a, false);
+	}
+}
+
+static void	prep_for_push(t_stack_node **stack, t_stack_node *top_node,
+					char stack_name)
+{
+	while (*stack != top_node)
+	{
+		if (stack_name == 'a')
+		{
+			if (top_node->above_median)
+				ra(stack, false);
+			else
+				rra(stack, false);
+		}
+		else if (stack_name == 'b')
+		{
+			if (top_node->above_median)
+				rb(stack, false);
+			else
+				rrb(stack, false);
+		}
+	}
+}
+
+// static void	move_b_to_a(t_stack_node **a, t_stack_node **b)
+// {
+// 	prep_for_push(a, (*b)->target_node, 'a');
+// 	pa(a, b, false);
+// }
+
+static void	move_b_to_a(t_stack_node **a, t_stack_node **b)
+{
+	t_stack_node	*cheapest_node;
+
+	cheapest_node = find_cheapest(*a);
+
+	if (cheapest_node->above_median && cheapest_node->target_node->above_median)
+		rotate_both(a, b, cheapest_node);
+	else if (!(cheapest_node->above_median)
+		&& !(cheapest_node->target_node->above_median))
+		rev_rotate_both(a, b, cheapest_node);
+	prep_for_push(a, cheapest_node, 'a');
+	prep_for_push(b, cheapest_node->target_node, 'b');
+	pa(a, b, false);
+}
+
+// void	sort_stacks(t_stack_node **a, t_stack_node **b)
+// {
+// 	int	len_a;
+
+// 	len_a = stack_len(*a);
+// 	if (len_a-- > 3 && !stack_sorted(*a))
+// 		pb(b, a, false);
+// 	if (len_a-- > 3 && !stack_sorted(*a))
+// 		pb(b, a, false);
+// 	while (len_a-- > 3 && !stack_sorted(*a))
+// 	{
+// 		init_nodes_a(*a, *b);
+// 		move_a_to_b(a, b);
+// 	}
+// 	sort_three(a);
+// 	while (*b)
+// 	{
+// 		init_nodes_b(*a, *b);
+// 		move_b_to_a(a, b);
+// 	}
+// 	current_index(*a);
+// 	min_to_top(a);
+// }
+
+// void	push_to_b(t_stack_node **a, t_stack_node **b, int len_a)
+// {
+		
+// }
+
+void	sort_stacks(t_stack_node **a, t_stack_node **b)
+{
+	int	len_a;
+
+	len_a = stack_len(*a);
+	push_to_b(b, a, len_a);
+	sort_three(a);
+	while (*b)
+	{
+		init_nodes_a(*a, *b);
+		move_b_to_a(a, b);
+	}
+	current_index(*a);
+	min_to_top(a);
+}
